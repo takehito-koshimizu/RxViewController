2fea5cdcbabab48159f3d34c45ae9649902a2556
Takehito KOSHIMIZU <takehito.koshimizu@glpgs.com>
2017-03-05 16:30:35 +0900

    Initial commit

---
+++ RxViewController/UIViewController+Reactive.swift
@@ -0,0 +1,55 @@
+//
+//  UIViewController+Reactive.swift
+//  RxViewController
+//
+//  Created by Takehito Koshimizu on 2017/03/05.
+//  Copyright © 2017年 Takehito Koshimizu. All rights reserved.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+public extension Reactive where Base: UIViewController {
+
+    public var viewWillAppear: Observable<Bool> {
+        return _sentMessage(#selector(base.viewWillAppear(_:)))
+    }
+
+    public var viewDidAppear: Observable<Bool> {
+        return _sentMessage(#selector(base.viewDidAppear(_:)))
+    }
+
+    public var viewWillDisappear: Observable<Bool> {
+        return _sentMessage(#selector(base.viewWillDisappear(_:)))
+    }
+
+    public var viewDidDisappear: Observable<Bool> {
+        return _sentMessage(#selector(base.viewDidDisappear(_:)))
+    }
+
+    public var viewWillLayoutSubviews: Observable<Void> {
+        return _sentMessage(#selector(base.viewWillLayoutSubviews))
+    }
+
+    public var viewDidLayoutSubviews: Observable<Void> {
+        return _sentMessage(#selector(base.viewDidLayoutSubviews))
+    }
+
+    public var prepareForSegue: Observable<UIViewController> {
+        let segue: Observable<UIStoryboardSegue> = _sentMessage(#selector(base.prepare(for:sender:)))
+        return segue.map { $0.destination }.shareReplay(1)
+    }
+}
+
+extension Reactive where Base: AnyObject {
+
+    func _sentMessage<T>(_ selector: Selector) -> Observable<T> {
+        return sentMessage(selector).map { ($0.first as? T)! }.shareReplay(1)
+    }
+
+    func _sentMessage(_ selector: Selector) -> Observable<Void> {
+        return sentMessage(selector).map { _ in () }.shareReplay(1)
+    }
+}
